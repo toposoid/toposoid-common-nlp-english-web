698dde9e86920fe85ee7dbb63d7553d321346480
makoto-kubodera <makoto-kubodera@linked-ideal.com>
2021-12-17 19:29:33 +0900

    first commit

---
+++ test_api.py
@@ -0,0 +1,42 @@
+'''
+  Copyright 2021 Linked Ideal LLC.[https://linked-ideal.com/]
+ 
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+ 
+      http://www.apache.org/licenses/LICENSE-2.0
+ 
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ '''
+from fastapi.testclient import TestClient
+from api import app
+from model import NormalizedWord
+import pytest
+
+#This is a unit test module
+client = TestClient(app)
+def test_EmptyWord():    
+    response = client.post("/getSynonyms",
+                        headers={"Content-Type": "application/json"},
+                        json={"word": ""})    
+    assert response.status_code == 200
+    assert response.json() == []
+
+def test_SimpleVerb():    
+    response = client.post("/getSynonyms",
+                        headers={"Content-Type": "application/json"},
+                        json={"word": "execute"})    
+    assert response.status_code == 200
+    assert response.json().sort() == ['accomplish', 'perform', 'execute'].sort()
+
+def test_SimpleNoun():    
+    response = client.post("/getSynonyms",
+                        headers={"Content-Type": "application/json"},
+                        json={"word": "agreement"})    
+    assert response.status_code == 200
+    assert response.json().sort() == ['accord', 'arrangement', 'agreement'].sort()
